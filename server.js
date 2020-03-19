3e038b27a34a7dde1121b051bfc233314fe10a61
tangxin <2417219614@qq.com>
2020-03-19 22:22:58 +0800

    使用Session,LocalStorage,Cache-Control

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ var http = require('http')
 var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
+var md5 = require('md5')
 
 if(!port){
     console.log('请指定端口奥好不好？\nnode server.js 8888 这样不会吗？')
     process.exit(1)
+}
+
+let sessions = {
+
 }
 
 var server = http.createServer(function(request, response){
@@ -19,10 +24,39 @@ var server = http.createServer(function(request, response){
 
     /*********从这里开始看，上面不要看*********/
     console.log('方方说：含查询字符串的路径为\n' + pathWithQuery)
-    
-    if(path === '/'){
-        let string = fs.readFileSync('./index.html', 'utf-8')  //同步读取网页文件
-        let cookies = request.headers.cookie.split('; ')
+    if(path === '/js/main.js'){
+        let string = fs.readFileSync('./js/main.js', 'utf8')
+        response.setHeader('Content-Type', 'application/javascript;charset=utf-8')
+        // 设置缓存时间
+        // response.setHeader('Cache-Control', 'max-age=30')
+        // response.setHeader('Expires', 'Thu, 19 Mar 2020 13:20:30 GMT')
+        // 拿到文件md5
+        let fileMd5 = md5(string)
+        response.setHeader('ETag', fileMd5)
+        if(request.headers['if-none-match'] === fileMd5){
+            // 304代表没改过，不会有响应体
+            response.statusCode = 304
+        }else{
+            // 返给响应头里面
+            response.setHeader('ETag', fileMd5)
+            // 有响应体
+            response.write(string)
+        }
+        response.end()
+    }else if(path === '/css/default.css'){
+        let string = fs.readFileSync('./css/default.css', 'utf8')
+        response.setHeader('Content-Type', 'text/css;charset=utf8')
+        response.setHeader('Cache-Control', 'max-age=30')
+        response.write(string)
+        response.end()
+    }else if(path === '/'){
+        let string = fs.readFileSync('./index.html', 'utf8')  //同步读取网页文件
+        // 将cookies放入解析成哈希
+        let cookies = []
+        if(request.headers.cookie){
+            cookies = request.headers.cookie.split('; ')
+        }
+        console.log(cookies)
         let hash = {}
         for(let i=0; i<cookies.length; i++){
             let parts = cookies[i].split('=')
@@ -30,7 +64,14 @@ var server = http.createServer(function(request, response){
             let value = parts[1]
             hash[key] = value
         }
-        let email = hash.sign_in_email
+
+        // 内存会被释放，我们判断一下如果有将将其赋值
+        let mySession = sessions[hash.sessionId]
+        let email
+        if(mySession){
+            email = mySession.sign_in_email
+        }
+
         let users = fs.readFileSync('./db/users', 'utf8')
         users = JSON.parse(users)
         let foundUser
@@ -47,16 +88,17 @@ var server = http.createServer(function(request, response){
         }
 
         response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.setHeader('Content-Type', 'text/html;charset=utf8')
         response.write(string)    // 将网页文件写入到response中返回
         response.end()
     }else if(path==='/sign_up' && method === 'GET'){
         let string = fs.readFileSync('./sign_up.html', 'utf-8')
         response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.setHeader('Content-Type', 'text/html;charset=utf8')
         response.write(string)
         response.end()
-    }else if(path==='/sign_up' && method === 'POST'){
+
+    }else if(path==='/sign_up' && method === 'POST'){    // 注册
         readBody(request).then((body)=>{
             let strings = body.split('&')    // ['email=1', 'password=2', 'password_confirmation=3']
             let hash = {}
@@ -74,7 +116,7 @@ var server = http.createServer(function(request, response){
             if(email.indexOf('@') === -1){
                 response.statusCode = 400
                 // 设置返回的格式，这样jQuery就可以自动识别了
-                response.setHeader('Content-Type', 'application/json;charset=utf-8')
+                response.setHeader('Content-Type', 'application/json;charset=utf8')
                 // 我们返回JSON数据，这样就可以实现选择性展示出错误信息，最外端的''不属于JSON，只表示是一个字符串
                 // JS可以理解JSON语法
                 response.write(`{
@@ -116,12 +158,12 @@ var server = http.createServer(function(request, response){
             response.end()
         })
     }else if(path === '/sign_in' && method === 'GET'){
-        let string = fs.readFileSync('./sign_in.html', 'utf-8')
+        let string = fs.readFileSync('./sign_in.html', 'utf8')
         response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.setHeader('Content-Type', 'text/html;charset=utf8')
         response.write(string)
         response.end()
-    }else if(path === '/sign_in' && method === 'POST'){
+    }else if(path === '/sign_in' && method === 'POST'){    // 登录
         readBody(request).then((body)=>{
             let strings = body.split('&')    // ['email=1', 'password=2', 'password_confirmation=3']
             let hash = {}
@@ -149,10 +191,13 @@ var server = http.createServer(function(request, response){
                     break
                 }
             }
-            console.log(found)
+
             if(found){
+                let sessionId = Math.random() * 100000
+                sessions[sessionId] = {sign_in_email: email}
                 // 设置cookie，其中HttpOnly就不允许用户使用JS更改cookie了
-                response.setHeader('Set-Cookie', `sign_in_email=${email}; HttpOnly`)
+                /*response.setHeader('Set-Cookie', `sessionId=${sessionId}`)*/ 
+                response.write(`{"sessionId": ${sessionId}}`)
                 response.statusCode = 200
             }else{
                 response.statusCode = 401
@@ -161,7 +206,7 @@ var server = http.createServer(function(request, response){
         })
     }else if(path==='/xxxx'){
         response.statusCode = 200
-        response.setHeader('Content-Type', 'text/json;charset=utf-8')
+        response.setHeader('Content-Type', 'text/json;charset=utf8')
         // 加一个后台的响应头，告诉浏览器，这个域名可以访问我的后台
         response.setHeader('Access-Control-Allow-Origin', 'http://frank.com:8001')
         // 这边其实返回的是一个字符串，这个字符串刚好符合JSON语法，此处绝对不是一个对象
@@ -179,12 +224,12 @@ var server = http.createServer(function(request, response){
     }else if(path='/main.js'){
         let string = fs.readFileSync('./main.js')  //同步读取网页文件
         response.statusCode = 200
-        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+        response.setHeader('Content-Type', 'text/javascript;charset=utf8')
         response.write(string)    // 将网页文件写入到response中返回
         response.end()
     }else{
         response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.setHeader('Content-Type', 'text/html;charset=utf8')
         response.write('呜呜呜')
         response.end()
     }
@@ -209,3 +254,5 @@ function readBody(request){
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
